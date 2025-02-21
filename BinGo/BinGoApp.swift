d221f12dd19d7cb941af12c41ab65190ee62b5c5
QodeWiz <154478861+QodeWiz@users.noreply.github.com>
2025-02-21 17:59:21 +1100

    Initial Commit

---
+++ BinGo/BinGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinGoApp.swift
+//  BinGo
+//
+//  Created by Ishrat Kaur on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct BinGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
