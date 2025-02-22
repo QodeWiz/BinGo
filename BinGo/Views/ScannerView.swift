b56355c95367579bd82cc23d18ea54a078db9ee2
QodeWiz <154478861+QodeWiz@users.noreply.github.com>
2025-02-22 19:39:39 +1100

    [Chore] Add .gitignore file

---
+++ BinGo/Views/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BinGo
+//
+//  Created by Ishrat Kaur on 22/2/2025.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
